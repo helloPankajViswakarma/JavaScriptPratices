3fe9df003536425c2444200af719bfb6ebbad0c7
unknown <hiipankajvishwakarma@gmail.com>
2025-11-25 12:50:46 +0530

    third Commit

--- function.js
+++ function.js
@@ -12,15 +12,39 @@
 // console.log(sayHello(5, 10));
 
 //parameter type checking 
-function addNumbers(num1, num2) {
-    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    //     throw new Error('Both arguments must be numbers');
-    // }
-    console.log(num1 + num2);
-    }
+// function addNumbers(num1, num2) {
+
+//     console.log(num1 + num2);
+//     }
 
     // addNumbers(5, 10);
     
     //arguments type checking
 
-    addNumbers(5,"10");
+
+//     function addNumbers(num1, num2) {
+//     // let result =num1 + num2;
+//     // console.log("result inside function",result);
+//     // return result;
+// return num1 + num2;   
+// }
+//    const result= addNumbers(5,40);
+//    console.log("result",result);
+
+
+function  loginUserMessage(usernamr="pankaj"){
+    // if (usernamr === undefined){
+    //     console.log( "Please provide a username");
+    //     return  
+    //   }
+    if(!usernamr)
+{
+        console.log( "Please provide a username");
+}
+        return `Welcome back, ${usernamr}!`;
+    
+    // return `Welcome back, ${usernamr}!`;
+}
+// const user =loginUserMessage();
+// const user =loginUserMessage("John");
+console.log(loginUserMessage());
