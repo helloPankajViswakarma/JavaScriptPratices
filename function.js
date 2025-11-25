644a93eb35cbc9af5cc92d4fc590321f544677aa
unknown <hiipankajvishwakarma@gmail.com>
2025-11-25 12:19:01 +0530

    Second Commit

---
+++ function.js
@@ -0,0 +1,26 @@
+// function helloWorld() {
+
+//     return "Hello, World!";
+// }
+
+// console.log(helloWorld());
+
+
+// function sayHello(a ,b) {
+//     return a + b;
+// }
+// console.log(sayHello(5, 10));
+
+//parameter type checking 
+function addNumbers(num1, num2) {
+    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    //     throw new Error('Both arguments must be numbers');
+    // }
+    console.log(num1 + num2);
+    }
+
+    // addNumbers(5, 10);
+    
+    //arguments type checking
+
+    addNumbers(5,"10");
